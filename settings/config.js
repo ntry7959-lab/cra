e9fb3f62dee2a4c1f95f4074f7f27067e56c1d9f
ntry7959-lab <ntry7959@gmail.com>
2025-09-15 13:57:23 +0500

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,4 +1,4 @@
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['6288708844767']
+global.owner = ['923205805029']
 global.gambar = "https://files.catbox.moe/5gp130.jpg"
 // GANTI NO OWNER DENGAN NOMOR MU LALU RUNN SEPERTI BIASA !!!
