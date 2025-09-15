c9e76319de58dce9470f589207a3e86b237ab325
ntry7959-lab <ntry7959@gmail.com>
2025-09-15 13:54:07 +0500

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,4 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6288708844767']
+global.gambar = "https://files.catbox.moe/5gp130.jpg"
+// GANTI NO OWNER DENGAN NOMOR MU LALU RUNN SEPERTI BIASA !!!
